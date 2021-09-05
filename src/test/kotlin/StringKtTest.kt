c0f813634952363ef71bec3b36dae713c1d82d03
jimxl <tianxiaxl@gmail.com>
2021-09-05 21:55:37 +0800

    删除了不需要的out目录

--- src/test/kotlin/StringKtTest.kt
+++ src/test/kotlin/StringKtTest.kt
@@ -10,7 +10,7 @@ class StringKtTest : DescribeSpec({
         }
 
         it("当有多个单词的时候，能去掉中间的下划线，并且每个单词的首字母大写") {
-            "abc_edf".camelize() shouldBe "AbcEdf"
+            "abc_eDf".camelize() shouldBe "AbcEdf"
         }
     }
 })

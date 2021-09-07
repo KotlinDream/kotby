eefe4679bb59778d646716b6c32c195c90e30012
jimxl <tianxiaxl@gmail.com>
2021-09-08 01:15:09 +0800

    添加了maven 的发布插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,13 @@ plugins {
     kotlin("jvm") version "1.5.30"
     id("org.sonarqube") version "3.3"
     jacoco
+    `java-library`
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "info.dreamcoder"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -33,10 +36,30 @@ tasks.jacocoTestReport {
     }
 }
 
+
 sonarqube {
     properties {
         property("sonar.projectKey", "KotlinDream_kotby")
         property("sonar.organization", "kotlin-dream")
         property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
     }
 }
